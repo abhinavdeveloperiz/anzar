6e234b2c071cf255152516b6ac756ee2f004f490
abhinavmabhi <dexmarketing192018@gmail.com>
2026-05-26 16:33:05 +0530

    last

--- hassan/urls.py
+++ hassan/urls.py
@@ -25,5 +25,12 @@ urlpatterns = [
     path('', views.index, name='index'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
+
+urlpatterns += static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT
+)
